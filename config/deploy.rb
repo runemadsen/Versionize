fac025d45acb862bc7bd27cd574181a3888e471a
Rune Skjoldborg Madsen <rune@runemadsen.com>
2011-02-02 16:17:29 -0500

    Capistrano worked

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,17 +1,25 @@
 set :application, "Versionize"
 set :repository,  "git@github.com:runemadsen/Versionize.git"
 
-set :deploy_to, "public_html/versionize.com"
+set :port, 30000
+set :deploy_to, "/home/rune/public_html/versionize.com"
 set :user, "rune"
 set :branch, "release"
 set :use_sudo, false
 #set :use_sudo, true
+set :deploy_via, :remote_cache
+
+set :scm_verbose, true
+
+# These solve errors, look here: http://weblog.rubyonrails.org/2007/10/14/capistrano-2-1
+default_run_options[:pty] = true
+default_run_options[:shell] = false
 
 set :scm, :git
 
-role :web, "rune@184.106.217.110:30000"
-role :app, "rune@184.106.217.110:30000"
-role :db, "rune@184.106.217.110:30000", :primary => true
+role :web, "184.106.217.110"
+role :app, "184.106.217.110"
+role :db, "184.106.217.110", :primary => true
 
 # Passenger stuff
 namespace :deploy do
