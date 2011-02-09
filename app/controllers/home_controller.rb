19c6baa2924a18e26f5fa0c2fdef644d2085e9aa
Rune Skjoldborg Madsen <rune@runemadsen.com>
2011-02-09 14:50:27 -0500

    Rearranged front page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,10 +1,13 @@
 class HomeController < ApplicationController
  
-  before_filter :require_user
+  #before_filter :require_user
  
   def index
-    # if logged in - redirect to 
-    # render the login screen
+    
+    unless current_user
+      redirect_to invites_url
+    end
+    
   end
  
 end
--- app/controllers/invites_controller.rb
+++ app/controllers/invites_controller.rb
@@ -1,5 +1,10 @@
 class InvitesController < ApplicationController
   
+  layout "front"
+  
+  def index
+  end
+  
   def create
     invite = Invite.new(:email => params[:invitemail])
     invite.save
