d569aa071c8c1aa944bbde0ccc9e7ec4a2a7088f
Rune Skjoldborg Madsen <rune@runemadsen.com>
2011-03-08 17:33:40 -0500

    Fixed issue 21 and 22

--- app/helpers/ideas_helper.rb
+++ app/helpers/ideas_helper.rb
@@ -1,2 +1,3 @@
 module IdeasHelper
+
 end
--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,14 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+
+// function remove_fields(link) 
+// {
+//   $(link).prev("input[type=hidden]").val("1");
+//   $(link).closest(".fields").hide();
+// }
+
+function add_formfield(link, content) 
+{
+	$('#extras').append(content);
+	return false;
+}
