da4097bd81d2c0fcd82ccc8ca03d664e94b27eaa
Rune Skjoldborg Madsen <rune@runemadsen.com>
2011-03-30 16:01:42 -0400

    New collaboratior works

--- app/controllers/collaborations_controller.rb
+++ app/controllers/collaborations_controller.rb
@@ -8,4 +8,28 @@ class CollaborationsController < ApplicationController
     @users = @idea.users
   end
   
+  def create
+    
+    user = User.find_by_email params[:email]
+    
+    unless user.nil?
+      collaboration = user.collaborations.find_by_idea_id params[:idea_id]
+      if collaboration.nil?
+        begin
+          idea = Idea.find params[:idea_id]
+          idea.collaborations.create! :user => user
+          flash[:notice] = "Yes sir, create collaboration"
+        rescue Exception => e
+          flash[:error] = "Something went wrong when trying to create collaboration: #{e}"
+        end
+      else
+        flash[:error] = "This user is already a collaborator on this idea"
+      end
+    else
+      flash[:error] = "The email adress is not registered on Versionize"
+    end
+    
+    redirect_to idea_collaborations_path(params[:idea_id])
+  end
+  
 end
