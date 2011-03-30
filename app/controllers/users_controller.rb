9daeacaeb91e894fc16d47e569fe92aeac368b4d
Rune Skjoldborg Madsen <rune@runemadsen.com>
2011-03-29 20:07:18 -0400

    Styled version boxes WITH css3 rotation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,8 @@ class UsersController < ApplicationController
   before_filter :require_no_user, :only => [:new, :create]
   before_filter :require_user, :only => [:show, :edit, :update]
   
+  layout "front", :only => [:new]
+  
   def new
     @user = User.new
   end
