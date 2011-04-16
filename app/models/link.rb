4cb02078d3e647d309e9049dd522ceb0ddcae021
Rune Skjoldborg Madsen <rune@runemadsen.com>
2011-04-16 17:52:57 -0400

    Added ability to insert videos from links

--- app/models/link.rb
+++ app/models/link.rb
@@ -7,4 +7,27 @@ class Link < Item
      File.extname(url)
   end
   
+  def vimeo?
+    url[0..15] == "http://vimeo.com" || url[0..19] == "http://www.vimeo.com"
+  end
+  
+  def vimeo_video_id
+    url.split("/").last.split("?")[0]
+  end
+  
+  def youtube?
+    url[0..21] == "http://www.youtube.com" || url[0..14] == "http://youtu.be" || url[0..17] == "http://youtube.com"
+  end
+  
+  def youtube_video_id
+    
+    url_split = url.split("?")
+    
+    if(url_split.count == 2)
+      CGI::parse(url_split[1])["v"]
+    else
+      url.split("/").last
+    end
+  end
+  
 end
