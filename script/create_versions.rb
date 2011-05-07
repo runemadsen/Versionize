8435ee31d3896194ba94f1b243da889fdf736547
Rune Skjoldborg Madsen <rune@runemadsen.com>
2011-05-07 15:53:45 -0400

    Changed script

--- script/create_versions.rb
+++ script/create_versions.rb
@@ -2,17 +2,20 @@ ideas = Idea.all
 
 ideas.each do |idea|
   
-  # create master branch
-  master = idea.versions.create(:name => "Original", :alias => "master")
+  begin
+    # create master branch
+    master = idea.versions.create(:name => "Original", :alias => "master")
   
-  # create all other branches
-  idea.repository.heads.each do |head|
+    # create all other branches
+    idea.repository.heads.each do |head|
     
-    if head.name != "master"
-      idea.versions.create(:name => head.name.capitalize, :alias => head.name, :parent_id => master.id)
+      if head.name != "master"
+        idea.versions.create(:name => head.name.capitalize, :alias => head.name, :parent_id => master.id)
+      end
     end
+  rescue Exception => e
+    puts "Error with idea: #{idea.id}: #{e}"
   end
-  
-  puts "Done"
-  
 end
+
+ puts "Done"
